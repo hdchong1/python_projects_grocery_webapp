fee84d7b8d518da0a20f6947e87439f99ecd1ce1
hdchong1 <hydchongca@gmail.com>
2022-02-11 18:08:26 -0500

    add rendering function in app.py

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/manage-product')
+def index():
+    return render_template('manage-product.html')
+
 if __name__ == "__main__":
     print("Starting Python Flask Server For Grocery Store Management System")
     app.run(port=5000)
